b083e42be431e5bdc02a1be1367cc1716818ea2a
Maura Slavin <MauraMSlavin@gmail.com>
2019-10-13 17:52:17 -0400

    initial commit: Home page displayed

---
+++ assets/Javascript/questions.js
@@ -0,0 +1,55 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  
+    {
+    title: "Camel hump variable names have capital letters as follows:",
+    choices: ["all capital letters", "no capital letters", "only the first letter capitalized", "some letters, but NOT the first, capitalized"],
+    answer: "some letters, but NOT the first, capitalized"
+  },
+
+    {
+    title: "The 'return' statemest does the following:",
+    choices: ["finishes running the function, then returns an argument", "immediately ceases running the function", "ends the function if certain conditions are met", "returns your last unwanted purchase from Walmrt"],
+    answer: "immediately ceases running the function"
+  },
+
+    {
+    title: "The selectQuery statement ______.",
+    choices: ["executes statemeents based on a conditional clause", "retrieves data from an html element", "asks the user a question", "retrieves data from a local Javascript object"],
+    answer: "retrieves data from an html element"
+  },
+
+    {
+    title: "The 'pwd' command in Git Bash",
+    choices: ["generates a random password", "resets your password", "displays the current directory", "lists items in the current director"],
+    answer: "displays the current directory"
+  },
+
+    {
+    title: "An html class ...",
+    choices: ["is a teaching tool.", "is a re-usable 'name' for an element used in css to stylize multiple html elements consistently.", "tracks how well a block of html code performs.", "delineates the stylish-ness of a website."],
+    answer: "is a re-usable 'name' for an element used in css to stylize multiple html elements consistently."
+  },
+
+    {
+    title: "'Float' in html is used ______.",
+    choices: ["to place objects on a web page.", "to prevent code from drowning.", "in adding/subracting real numbers.", "none of the above."],
+    answer: "to place objects on a web page."
+  },
+
+    {
+    title: "The ____ domain can be used to watch variables and set breakpoints when debugging.",
+    choices: ["console", "source", "inspect", "none of the above"],
+    answer: "source"
+  },
+];
