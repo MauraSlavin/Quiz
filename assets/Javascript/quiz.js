a9996cd065b487cdb089e92b67ba727641827d43
Maura Slavin <MauraMSlavin@gmail.com>
2019-10-16 12:55:37 -0400

    Displays first question

--- assets/Javascript/quiz.js
+++ assets/Javascript/quiz.js
@@ -1,27 +1,70 @@
 var timeLimit = 135; // 15 secs per question, 9 questions
 var penaltyTime = 15; // lose 15 seconds for each wrong question.
 var timer = timeLimit; // start timer at timeLimit (time allotted).
-var startQuiz = document.querySelector("#beginQuiz");
+var $startQuiz = $("#beginQuiz");
+var message = "You have 15 seconds for each question, and will be penalized 15 seconds for each wrong answer.  Your score is the time left when you complete the quiz.";
+var instructions = "Click on the correct answer:";
+var currentQuestion = 0;
 
-// startQuiz = JSON.parse(startQuiz)
-console.log("startQuiz: " + startQuiz);
+// $startQuiz = JSON.parse($startQuiz);
+console.log("begin quiz.js");
+console.log("")
 
-startQuiz.addEventListener("click", runQuiz(event));
+$(document).ready(function () {
+  // when take quiz button pressed..
+  $("#beginQuiz").click(
 
-function runQuiz(event) {
-  //  event.preventDefault();
-for (var q = 0; q < questions.length; q++) {
- //    console.log("Question" + q + " is " + questions[q].title);
- // renderQuizPage(q);
- console.log("question: " + q);
- 
-}
-}
+    function () {
+      $("#beginQuiz").remove();  // remove "take quiz button"
+      $("#message").text(message);  // change the message on the page
+      //  console.log("begin runQuiz");
+
+      //// for each question...
+      //  questions.forEach(function (question) {  
+
+      //   ************  just do for first one, for now
+      var question = questions[currentQuestion];  // for testing  ********************
+        console.log("Questions (in forEach loop): " + question);
+        // var thisQuestion = questions.title;
+        
+        var thisQuestion = question.title;
+        console.log("Question (question.title) is:  " + thisQuestion);
+        $("#question").text(thisQuestion);   // display the question
+        question.choices.forEach(function (choice, index) {   // display the choices
+          console.log("choice:  " + choice);
+          console.log("index: " + index);
+          //  var thisChoiceIndex = "#choice" + index;
+          $("#" + index).text(choice);
+        });
+        //// listen for answer
+        //// evaluate answer
+        //// respond to answer
+        //// display next question
+
+        // finish taking quiz
+        //  display score
+
+        //// then - add timer and high score functions
+
+
+      });    // end click on beginQuiz
+
+    });  // end listening for document ready
 
 
 
-// function displayWelcomePage() {
-//  }
+  //  $("#beginQuiz").on("click", runQuiz(event));
+
+  //   event.preventDefault();
+
+
+  console.log("Timer: " + timer);
+  console.log("end of iteration\n\n");
+
+
+
+
+
 
 
 
